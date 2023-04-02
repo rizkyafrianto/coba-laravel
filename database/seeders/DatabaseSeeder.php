a3afd10266e315ccf78b991a04d4905946fcae63
Rizky Afrianto <rizky.afrianto039@gmail.com>
2023-04-02 19:52:23 +0700

    Menggunakan fitur Factory dan membuat faker

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,24 +15,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
 
-        User::create([
-            'name' => 'Afrianto',
-            'email' => 'afrianto@gmail.com',
-            'password' => bcrypt('12345')
-        ]);
-
-        User::create([
-            'name' => 'Farhan',
-            'email' => 'farhan@gmail.com',
-            'password' => bcrypt('12345')
-        ]);
+        \App\Models\User::factory(5)->create();
 
         Category::create([
             'name' => 'Web Programming',
@@ -49,40 +38,6 @@ class DatabaseSeeder extends Seeder
             'slug' => 'web design'
         ]);
 
-        Post::create([
-            'title' => 'Judul Pertama',
-            'slug' => 'judul-pertama',
-            'category_id' => 1,
-            'user_id' => 1,
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat aliquam ducimus perspiciatis recusandae atque facilis magni culpa officia repellendus obcaecati,',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat aliquam ducimus perspiciatis recusandae atque facilis magni culpa officia repellendus obcaecati, nesciunt tenetur explicabo. Dolore maxime ullam totam asperiores qui eveniet, debitis sint perferendis unde, mollitia quibusdam optio tempore, eius rem modi natus rerum aut pariatur quae esse explicabo. Itaque illo velit omnis ut, hic minus tenetur quidem! Praesentium inventore suscipit quidem, dolore enim, eum vel quam fugiat molestias facilis harum doloribus. Animi, commodi modi id fugiat repudiandae error, repellendus accusantium quos sapiente, dicta laboriosam. Non autem voluptates voluptatibus ipsam vero tenetur, facilis et molestias modi commodi. Ducimus ea vitae sapiente quaerat, nostrum reprehenderit voluptas optio suscipit commodi nihil necessitatibus. Eius optio mollitia voluptatibus! Molestias, omnis? Cumque autem ipsa voluptates, officia voluptate molestiae libero voluptas nobis quia, inventore deleniti aperiam voluptatem odio beatae nam placeat nihil modi neque ipsum architecto, eveniet voluptatum animi qui? Ipsa cum doloremque corrupti, totam beatae eius.'
-        ]);
-
-        Post::create([
-            'title' => 'Judul Kedua',
-            'slug' => 'judul-kedua',
-            'category_id' => 1,
-            'user_id' => 1,
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat aliquam ducimus perspiciatis recusandae atque facilis magni culpa officia repellendus obcaecati,',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat aliquam ducimus perspiciatis recusandae atque facilis magni culpa officia repellendus obcaecati, nesciunt tenetur explicabo. Dolore maxime ullam totam asperiores qui eveniet, debitis sint perferendis unde, mollitia quibusdam optio tempore, eius rem modi natus rerum aut pariatur quae esse explicabo. Itaque illo velit omnis ut, hic minus tenetur quidem! Praesentium inventore suscipit quidem, dolore enim, eum vel quam fugiat molestias facilis harum doloribus. Animi, commodi modi id fugiat repudiandae error, repellendus accusantium quos sapiente, dicta laboriosam. Non autem voluptates voluptatibus ipsam vero tenetur, facilis et molestias modi commodi. Ducimus ea vitae sapiente quaerat, nostrum reprehenderit voluptas optio suscipit commodi nihil necessitatibus. Eius optio mollitia voluptatibus! Molestias, omnis? Cumque autem ipsa voluptates, officia voluptate molestiae libero voluptas nobis quia.'
-        ]);
-
-        Post::create([
-            'title' => 'Judul Ketiga',
-            'slug' => 'judul-ketiga',
-            'category_id' => 2,
-            'user_id' => 1,
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat aliquam ducimus perspiciatis recusandae atque facilis magni culpa officia repellendus obcaecati,',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat aliquam ducimus perspiciatis recusandae atque facilis magni culpa officia repellendus obcaecati, nesciunt tenetur explicabo. Dolore maxime ullam totam asperiores qui eveniet, debitis sint perferendis unde, mollitia quibusdam optio tempore, eius rem modi natus rerum aut pariatur quae esse explicabo. Itaque illo velit omnis ut, hic minus tenetur quidem! Praesentium inventore suscipit quidem, dolore enim, eum vel quam fugiat molestias facilis harum doloribus. Animi, commodi modi id fugiat repudiandae error, repellendus accusantium quos sapiente, dicta laboriosam. Non autem voluptates voluptatibus ipsam vero tenetur, facilis et molestias modi commodi. Ducimus ea vitae sapiente quaerat, nostrum reprehenderit voluptas optio suscipit commodi nihil necessitatibus. Eius optio mollitia voluptatibus! Molestias, omnis? Cumque autem ipsa voluptates, officia voluptate molestiae libero voluptas nobis quia.'
-        ]);
-
-        Post::create([
-            'title' => 'Judul Keempat',
-            'slug' => 'judul-keempat',
-            'category_id' => 3,
-            'user_id' => 2,
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat aliquam ducimus perspiciatis recusandae atque facilis magni culpa officia repellendus obcaecati,',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat aliquam ducimus perspiciatis recusandae atque facilis magni culpa officia repellendus obcaecati, nesciunt tenetur explicabo. Dolore maxime ullam totam asperiores qui eveniet, debitis sint perferendis unde, mollitia quibusdam optio tempore, eius rem modi natus rerum aut pariatur quae esse explicabo. Itaque illo velit omnis ut, hic minus tenetur quidem! Praesentium inventore suscipit quidem, dolore enim, eum vel quam fugiat molestias facilis harum doloribus. Animi, commodi modi id fugiat repudiandae error, repellendus accusantium quos sapiente, dicta laboriosam. Non autem voluptates voluptatibus ipsam vero tenetur, facilis et molestias modi commodi. Ducimus ea vitae sapiente quaerat, nostrum reprehenderit voluptas optio suscipit commodi nihil necessitatibus. Eius optio mollitia voluptatibus! Molestias, omnis? Cumque autem ipsa voluptates, officia voluptate molestiae libero voluptas nobis quia.'
-        ]);
+        \App\Models\Post::factory(15)->create();
     }
 }
--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -4,11 +4,13 @@
     <h5 class="my-3">Category: {{ $category }} </h5>
 
     @foreach ($posts as $post)
-        <article>
+        <article class="mb-5 border-bottom pb-4">
             <h2>
                 <a href="/blog/{{ $post->slug }}" class="text-decoration-none">{{ $post->title }}</a>
-            </h2>
+            </h2><a href="/categories/{{ $post->category->slug }}"
+                class="text-decoration-none">{{ $post->category->name }}</a></p>
             <p>{{ $post->excerpt }}</p>
+            <a href="/blog/{{ $post->slug }}" class="text-decoration-none">Read more...</a>
         </article>
     @endforeach
 @endsection
