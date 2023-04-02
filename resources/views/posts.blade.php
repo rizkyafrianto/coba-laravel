3df9a1ec97118e9a13e6e1a24d1d49a4f87d6022
Rizky Afrianto <rizky.afrianto039@gmail.com>
2023-04-02 08:49:12 +0700

    testing commit

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.main')
 
 @section('container')
-    <h1 class="my-3">Halaman Blog Posts</h1>
+    <h1 class="my-3">Blog Posts Page</h1>
 
     @foreach ($posts as $post)
         <article>
